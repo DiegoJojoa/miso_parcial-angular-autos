65d2f8eec6658a35d1ecf0ece11e296970f007b1
DiegoJojoa <d.jojoas@uniandes.edu.co>
2025-10-31 13:43:33 -0600

    feat: contador lista por marca

--- src/app/vehiculos/vehiculos-list/vehiculos-list.component.ts
+++ src/app/vehiculos/vehiculos-list/vehiculos-list.component.ts
@@ -6,19 +6,33 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-vehiculos-list',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule],
   templateUrl: './vehiculos-list.component.html',
-  styleUrls: ['./vehiculos-list.component.css']
+  styleUrls: ['./vehiculos-list.component.css'],
 })
 export class VehiculosListComponent implements OnInit {
   vehiculos: Vehiculo[] = [];
+  contadorListaPorMarca: Array<[string, number]> = [];
 
   constructor(private vehiculosService: VehiculosService) {}
 
   ngOnInit(): void {
     this.vehiculosService.getVehiculos().subscribe({
-      next: (data) => (this.vehiculos = data),
-      error: (e) => console.error('Error cargando vehículos', e)
+      next: (data) => {
+        this.vehiculos = data;
+
+        // Calcula totales por marca
+        const counts: Record<string, number> = data.reduce((acc, v) => {
+          acc[v.marca] = (acc[v.marca] ?? 0) + 1;
+          return acc;
+        }, {} as Record<string, number>);
+
+        // Se ordena alfabéticamente por marca
+        this.contadorListaPorMarca = Object.entries(counts).sort((a, b) =>
+          a[0].localeCompare(b[0])
+        );
+      },
+      error: (e) => console.error('Error cargando vehículos', e),
     });
   }
 }
